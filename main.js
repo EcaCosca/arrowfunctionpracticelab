49bfe7ae632d7280854ffb1a2f9c053e2b84ba00
aalsbr <a.alsbr40@gmail.com>
2021-10-24 13:26:24 +0300

    Arrow Functions done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // FUNCTION ONE 
-function sum(num1, num2){
+const sum=(num1, num2)=>{
     return num1 + num2
 }
 
@@ -9,7 +9,7 @@ console.log("the answer to everything is", sum(42,0))
 
 
 // FUNCTION TWO 
-function stringLength(str){
+const stringLength=(str)=>{
     console.log(`the length of "${str}" is:`, str.length)
 }
 
@@ -19,19 +19,19 @@ stringLength(longestCityNameInTheWorld)
 
 
 // FUNCTION THREE
-function stringLength(str){
+const stringLength2=(str)=>{
     let length = str.length
     console.log(`the length of "${str}" is:`, length)
     return str.length
 }
 
-stringLength("willynilly")
+stringLength2("willynilly")
 
 
 // FUNCTION FOUR
 let alerts = ["Hey, you are awesome", "You are so wonderful", "What a marvel you are", "You're so lovely", "You're so sweet that I'd think you're a sweet potato -- and I LOOOOVE POTATOES"]
 
-function showAlert(name){    
+const showAlert=(name)=>{    
     alert(alerts[(Math.floor(Math.random()*alerts.length))] + `, ${name}!`)
 }
 
