6afa6922c9515049a58ccad3b1764fbcde83bd96
0Yasser <yasiirh@gmail.com>
2021-10-24 13:41:48 +0300

    First commit

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // FUNCTION ONE 
-function sum(num1, num2){
-    return num1 + num2
-}
+let sum = (num1,num2) => num1 + num2;
 
 sum(40,2)
 sum(42,0)
@@ -9,9 +7,7 @@ console.log("the answer to everything is", sum(42,0))
 
 
 // FUNCTION TWO 
-function stringLength(str){
-    console.log(`the length of "${str}" is:`, str.length)
-}
+let stringLength = str => {console.log(`the length of "${str}" is:`, str.length);}
 
 let longestCityNameInTheWorld = "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu"
 
@@ -19,19 +15,19 @@ stringLength(longestCityNameInTheWorld)
 
 
 // FUNCTION THREE
-function stringLength(str){
+let stringLength1 = (str) => {
     let length = str.length
     console.log(`the length of "${str}" is:`, length)
     return str.length
 }
 
-stringLength("willynilly")
+stringLength1("willynilly")
 
 
 // FUNCTION FOUR
 let alerts = ["Hey, you are awesome", "You are so wonderful", "What a marvel you are", "You're so lovely", "You're so sweet that I'd think you're a sweet potato -- and I LOOOOVE POTATOES"]
 
-function showAlert(name){    
+let showAlert = name => {    
     alert(alerts[(Math.floor(Math.random()*alerts.length))] + `, ${name}!`)
 }
 
